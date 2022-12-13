6cd927335a239bcfa0f983283b61c68b00127c21
ManoelPidoux <64637435+ManoelPidoux@users.noreply.github.com>
2022-12-13 14:52:28 +0100

    Update app2 by adding the code for the spider chart

--- app2.R
+++ app2.R
@@ -1,21 +1,88 @@
 library(shiny)
+library(readxl)
+library(fmsb)
+
+Convert <- function(jumping = 0, ball_toss = 0, equilibrium = 0, planking = 0, running = 0){
+  if(any(!is.numeric(jumping))){
+    stop("'jumping' must be numeric")
+  }
+  if(any(!is.numeric(ball_toss))){
+    stop("'ball_toss' must be numeric")
+  }
+  if(any(!is.numeric(equilibrium))){
+    stop("'equilibrium' must be numeric")
+  }
+  if(any(!is.numeric(planking))){
+    stop("'planking' must be numeric")
+  }
+  if(any(!is.numeric(running))){
+    stop("'running' must be numeric")
+  }
+  if(any(jumping < 0)){
+    stop("'jumping' must be positive")
+  }
+  if(any(ball_toss<0)){
+    stop("'ball_toss' must be positive")
+  }
+  if(any(equilibrium<0)){
+    stop("'equilibrium' must be positive")
+  }
+  if(any(planking<0)){
+    stop("'planking' must be positive")
+  }
+  if(any(running<0)){
+    stop("'running' must be positive")
+  }
+  points_army <- readxl::read_excel(here::here("Data/points_army.xlsx"))
+  list_of_points <- list(jp = 0, btp = 0, ep = 0, pp = 0, rp = 0)
+  if(jumping < points_army$jumping[25]){list_of_points$jp = points_army$points[26]}
+  if(jumping >= points_army$jumping[1]){list_of_points$jp = points_army$points[1]}
+  for(i in (nrow(points_army)-1):2){
+    if((jumping < points_army$jumping[i-1])&(jumping >= points_army$jumping[i])){list_of_points$jp = points_army$points[i]}
+  }
+  if(ball_toss < points_army$ball_toss[25]){list_of_points$btp = points_army$points[26]}
+  if(ball_toss >= points_army$ball_toss[1]){list_of_points$btp = points_army$points[1]}
+  for(i in (nrow(points_army)-1):2){
+    if((ball_toss < points_army$ball_toss[i-1])&(ball_toss >= points_army$ball_toss[i])){list_of_points$btp = points_army$points[i]}
+  }
+  if(equilibrium < points_army$equilibrium[25]){list_of_points$ep = points_army$points[26]}
+  if(equilibrium >= points_army$equilibrium[1]){list_of_points$ep = points_army$points[1]}
+  for(i in (nrow(points_army)-1):2){
+    if((equilibrium < points_army$equilibrium[i-1])&(equilibrium >= points_army$equilibrium[i])){list_of_points$ep = points_army$points[i]}
+  }
+  if(planking < points_army$planking[25]){list_of_points$pp = points_army$points[26]}
+  if(planking >= points_army$planking[1]){list_of_points$pp = points_army$points[1]}
+  for(i in (nrow(points_army)-1):2){
+    if((planking < points_army$planking[i-1])&(planking >= points_army$planking[i])){list_of_points$pp = points_army$points[i]}
+  }
+  if(running < points_army$running[25]){list_of_points$rp = points_army$points[26]}
+  if(running >= points_army$running[1]){list_of_points$rp = points_army$points[1]}
+  for(i in (nrow(points_army)-1):2){
+    if((running < points_army$running[i-1])&(running >= points_army$running[i])){list_of_points$rp = points_army$points[i]}
+  }
+  data <- as.data.frame(list_of_points)
+  colnames(data) <- c("jumping" , "ball toss" , "equilibrium" , "planking" , "running" )
+  data <- rbind(rep(25,5) , rep(0,5) , data)
+  fmsb::radarchart(data, axistype=1 ,
+                   pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 ,
+                   cglcol="grey", cglty=1, axislabcol="black", caxislabels=c(0,NA,12.5, NA,25), cglwd=0.8,
+                   vlcex=0.8)
+  return(invisible(list_of_points))
+}
 
 ui <- fluidPage(
   # App title ----
   titlePanel("Performance Scoreboard for Recruitment"),
-
-
-
   # Slider 1
   fluidRow(
     column(4,
            img(src = "armee.png", height=70, width=400)
     ),
     column(3,
-           sliderInput(inputId = "bins",
+           sliderInput(inputId = "Jump",
                        label = "Jumping:",
                        min = 0,
-                       max = 2.85,
+                       max = 3.1,
                        value = 30)
     ),
     column(4,
@@ -28,10 +95,10 @@ ui <- fluidPage(
            img(src = "armee.png", height=70, width=400)
     ),
     column(3,
-           sliderInput(inputId = "bins",
+           sliderInput(inputId = "Ball",
                        label = "Ball Toss:",
                        min = 0,
-                       max = 8.5,
+                       max = 10.1,
                        value = 30)
     ),
     column(4,
@@ -44,10 +111,10 @@ ui <- fluidPage(
            img(src = "armee.png", height=70, width=400)
     ),
     column(3,
-           sliderInput(inputId = "bins",
+           sliderInput(inputId = "Equil",
                        label = "Equilibrium:",
                        min = 0,
-                       max = 100,
+                       max = 110,
                        value = 30)
     ),
     column(4,
@@ -60,10 +127,10 @@ ui <- fluidPage(
            img(src = "armee.png", height=70, width=400)
     ),
     column(3,
-           sliderInput(inputId = "bins",
+           sliderInput(inputId = "Plank",
                        label = "Planking:",
                        min = 0,
-                       max = 290,
+                       max = 300,
                        value = 30)
     ),
     column(4,
@@ -76,15 +143,19 @@ ui <- fluidPage(
            img(src = "armee.png", height=70, width=400)
     ),
     column(3,
-           sliderInput(inputId = "bins",
-                       label = "Planking:",
+           sliderInput(inputId = "Run",
+                       label = "Runing:",
                        min = 0,
-                       max = 290,
+                       max = 1300,
                        value = 30)
     ),
     column(4,
            img(src = "armee.png", height=70, width=400)
     )
+  ),
+  # row 6
+  fluidPage(
+    plotOutput("plot", width = "400px")
   )
 )
 
@@ -105,6 +176,9 @@ server <- function(input, output) {
   })
   output$slider3Value <- renderText({
     input$slider5
+  })
+  output$plot <- renderPlot({
+    Convert(jumping = input$Jump, ball_toss = input$Ball, equilibrium = input$Equil, planking = input$Plank, running = input$Run )
   })
 }
 
