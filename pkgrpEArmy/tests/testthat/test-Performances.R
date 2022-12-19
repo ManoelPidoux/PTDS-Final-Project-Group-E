cd67a2a8115d7642e9ca679feba761d38d54c605
ManoelPidoux <64637435+ManoelPidoux@users.noreply.github.com>
2022-12-19 22:58:53 +0100

    Cleaning

--- pkgrpEArmy/tests/testthat/test-Performances.R
+++
@@ -1,7 +0,0 @@
-test_that("Calling Performances will return the homonymus data-set", {
-  expect_equal(Performances, Performances)
- })
-
-test_that("Calling Performances('A') will return a message of error", {
-  expect_error(Performances('A'))
-})
